13536e4c16ced6115c33841b134f41224b12eb7b
Aaron Alberto Alva Caffo <u201719762@upc.edu.pe>
2020-10-01 16:02:08 -0500

    Controller UserNoChef

--- src/main/java/com/acme/homemade/demo/controller/UserNoChefController.java
+++ src/main/java/com/acme/homemade/demo/controller/UserNoChefController.java
@@ -4,15 +4,15 @@ import com.acme.homemade.demo.domain.model.UserNoChef;
 import com.acme.homemade.demo.domain.service.UserNoChefService;
 import com.acme.homemade.demo.resource.SaveUserNoChefResource;
 import com.acme.homemade.demo.resource.UserNoChefResource;
+import io.swagger.v3.oas.annotations.Operation;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +25,7 @@ public class UserNoChefController {
     @Autowired
     private UserNoChefService userNoChefService;
 
+    @Operation(summary = "Get All userNoChefs", description = "Get All available userNoChefs", tags = {"userNoChefs"})
     @GetMapping("/userNoChefs")
     public Page<UserNoChefResource> getAllUserNoChef(Pageable pageable) {
         Page<UserNoChef> userNoChef = userNoChefService.getAllUserNoChef(pageable);
@@ -35,6 +36,24 @@ public class UserNoChefController {
         return new PageImpl<>(resources, pageable, resources.size());
     }
 
+    @Operation(summary = "Get userNoChefs By Id", description = "Get userNoChefs for given Id", tags = {"userNoChefs"})
+    @GetMapping("/userNoChefs/{id}")
+    public UserNoChefResource getUserNoChefById(@PathVariable(name = "id") Long id){
+        return convertToResource(userNoChefService.getUserNoChefById(id));
+    }
+
+    @Operation(summary = "Create userNoChefs", description = "Create a new userNoChefs", tags = {"userNoChefs"})
+    @PostMapping("/userNoChefs")
+    public UserNoChefResource createUserNoChef(@Valid @RequestBody SaveUserNoChefResource resource){
+        return convertToResource(userNoChefService.createUserNoChef(convertToEntity(resource)));
+    }
+
+    @Operation(summary = "Update userNoChefs", description = "Update userNoChefs with given Id", tags = {"userNoChefs"})
+    @PutMapping("/userNoChefs/{id}")
+    public UserNoChefResource updateUserNoChefResource(@PathVariable(name = "id") Long userNoChefId,
+                                                       @Valid @RequestBody SaveUserNoChefResource resource){
+        return convertToResource(userNoChefService.updateUserNoChef(userNoChefId,convertToEntity(resource)));
+    }
 
     private UserNoChef convertToEntity(SaveUserNoChefResource resource) {
         return mapper.map(resource, UserNoChef.class);
