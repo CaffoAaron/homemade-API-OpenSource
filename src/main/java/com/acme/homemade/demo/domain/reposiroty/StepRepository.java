3d6d45eb46cf7e1dd27ed67057acd632b57176d8
PSTQ <PSTQ@examen.com.pe>
2020-10-03 02:26:21 -0500

    Actualización Step
    StepRepository actualizado

--- src/main/java/com/acme/homemade/demo/domain/reposiroty/StepRepository.java
+++ src/main/java/com/acme/homemade/demo/domain/reposiroty/StepRepository.java
@@ -1,4 +1,18 @@
 package com.acme.homemade.demo.domain.reposiroty;
 
-public interface StepRepository {
+import com.acme.homemade.demo.domain.model.Step;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+ import java.util.Optional;
+
+ public interface StepRepository extends JpaRepository<Step, Long>{
+
+       Page<Step> findById (Long Id, Pageable pageable);
+
+       Page<Step> findByRecipe_Id (Long recipeId, Pageable pageable);
+
+
+       Optional<Step> findByIdAndRecipe_Id (Long Id, Long recipe_Id);
 }
