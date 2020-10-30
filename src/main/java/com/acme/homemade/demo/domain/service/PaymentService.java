5426ad26388e2dcc7a7513ad54550c32516a9093
SanchezD1998 <44213289+SanchezD1998@users.noreply.github.com>
2020-10-30 13:08:59 -0500

    delete payment

--- src/main/java/com/acme/homemade/demo/domain/service/PaymentService.java
+++ src/main/java/com/acme/homemade/demo/domain/service/PaymentService.java
@@ -1,10 +1,5 @@
 package com.acme.homemade.demo.domain.service;
 
 public interface PaymentService {
-    Page<Payment> getAllPaymentByUserNoChefId(Long userNoChefId, Pageable pageable);
-    Page<Payment> getAllPayment (Pageable pageable);
-    Payment getPaymentByIdAndUserNoChefId (Long userNoChefId, Long paymentId);
-    Payment createPayment(Long userNoChefId, Payment payment);
-    Payment updatePayment(Long userNoChefId, Long paymentId ,Payment payment);
-    ResponseEntity<?>  deletePayment (Long userNoChefId, Long paymentId);
+    
 }
