7258db64ce9f86e7374245d394c11c2a7e59d272
PerKimba <35297513+PerKimba@users.noreply.github.com>
2020-10-29 20:20:48 -0500

    Fixes

--- src/main/java/com/acme/homemade/demo/domain/service/PaymentService.java
+++ src/main/java/com/acme/homemade/demo/domain/service/PaymentService.java
@@ -3,15 +3,15 @@ package com.acme.homemade.demo.domain.service;
 public interface PaymentService {
     Payment getPaymentById (Long paymentId);
 
-    Page<Payment> getAllPaymentByUserNoChefId(Long userId, Pageable pageable);
+    Page<Payment> getAllPaymentByUserNoChefId(Long userNoChefId, Pageable pageable);
 
     Page<Payment> getAllPayment (Pageable pageable);
 
-    Payment getPaymentByIdAndUserNoChefId (Long userId, Long paymentId);
+    Payment getPaymentByIdAndUserNoChefId (Long userNoChefId, Long paymentId);
 
-    Payment createPayment(Long userId, Payment payment);
+    Payment createPayment(Long userNoChefId, Payment payment);
 
-    Payment updatePayment(Long userId, Long paymentId ,Payment payment);
+    Payment updatePayment(Long userNoChefId, Long paymentId ,Payment payment);
 
-    ResponseEntity<?>  deletePayment (Long userId, Long paymentId);
+    ResponseEntity<?>  deletePayment (Long userNoChefId, Long paymentId);
 }
