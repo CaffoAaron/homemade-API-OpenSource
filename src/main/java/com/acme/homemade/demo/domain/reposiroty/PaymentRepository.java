7d2a3f23e045de032821a23d9ab26a2eaf94b1c4
PerKimba <35297513+PerKimba@users.noreply.github.com>
2020-10-29 20:17:58 -0500

    Revert "Revert "Payments""
    This reverts commit 7b0670140b3550e8155ca0b743ea2a8a5ebd2b66.

--- src/main/java/com/acme/homemade/demo/domain/reposiroty/PaymentRepository.java
+++ src/main/java/com/acme/homemade/demo/domain/reposiroty/PaymentRepository.java
@@ -1,6 +1,5 @@
 package com.acme.homemade.demo.domain.reposiroty;
 
-
 import com.acme.homemade.demo.domain.model.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,10 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long>{
-    Page<Payment> findById (Long Id, Pageable pageable);
-
-    Page<Payment> findByUserNoChefId (Long userNoChefId, Pageable pageable);
-
-    Optional<Payment> findByIdAndUserNoChefId (Long Id, Long userNoChefId);
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Page<Payment> findById(Long Id, Pageable pageable);
+    Page<Payment> findByUserNoChefId(Long userNoChefId, Pageable pageable);
+    Optional<Payment> findByIdAndUserNoChefId(Long Id, Long userNoChefId);
 }
