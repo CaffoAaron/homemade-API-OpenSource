54a91a913d519f71db1c90a38a332f58c3aa7629
PerKimba <35297513+PerKimba@users.noreply.github.com>
2020-10-29 20:01:35 -0500

    Payments

--- src/main/java/com/acme/homemade/demo/domain/service/PaymentService.java
+++ src/main/java/com/acme/homemade/demo/domain/service/PaymentService.java
@@ -1,4 +1,17 @@
 package com.acme.homemade.demo.domain.service;
 
 public interface PaymentService {
+    Payment getPaymentById (Long paymentId);
+
+    Page<Payment> getAllPaymentByUserNoChefId(Long userId, Pageable pageable);
+
+    Page<Payment> getAllPayment (Pageable pageable);
+
+    Payment getPaymentByIdAndUserNoChefId (Long userId, Long paymentId);
+
+    Payment createPayment(Long userId, Payment payment);
+
+    Payment updatePayment(Long userId, Long paymentId ,Payment payment);
+
+    ResponseEntity<?>  deletePayment (Long userId, Long paymentId);
 }
