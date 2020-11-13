841d87173ea3fc6128c041241add09a63bb069d4
PerKimba <35297513+PerKimba@users.noreply.github.com>
2020-11-13 15:37:26 -0500

    Update PaymentController.java

--- src/main/java/com/acme/homemade/demo/controller/PaymentController.java
+++ src/main/java/com/acme/homemade/demo/controller/PaymentController.java
@@ -2,6 +2,8 @@ package com.acme.homemade.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 
 @RestController
 @RequestMapping("/api")
